30601ad348f4fa852ebfbb53ddfaa7009671d8a2
Kerubin Nemes <karelisho@gmail.com>
2023-03-13 09:57:26 +0100

    g

--- Könyvtár/App_Start/RouteConfig.cs
+++ Könyvtár/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Könyvtár
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Default",  action = "Index", id = UrlParameter.Optional }
+                defaults: new {controller = "Default",  action = "Main", id = UrlParameter.Optional }
             );
         }
     }
